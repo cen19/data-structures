39f0836ad5ec27f37176befb86ba0cd5b950deac
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:20:56 -0700

    Finished Prototypal Q

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.queueSize = 0; // size of the queue
+  newQueue.firstPlace = -1; // position of 'first person'
+  newQueue.nextQueuePerson = 0;
+  
+  return newQueue;
+  
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.nextQueuePerson] = value;
+  this.queueSize++;
+  this.nextQueuePerson++;
+};
 
+queueMethods.dequeue = function() {
+  // returns the next person whom will leave the queue
+  this.queueSize--;
+  this.firstPlace++;
+  return this.storage[this.firstPlace];
+};
+
+// can't have newQueue key's have the same name as queueMethod's key
+queueMethods.size = function() {
+  return this.queueSize > 0 ? this.queueSize : 0;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // need a newStack to store methods
+    // create with Objects.create(**insert stackMethods here**)
+  var newStack = Object.create(stackMethods);
+  // declare shared 'variables'
+    // in our case, definitions of the property
+  newStack.count = 0;
+  newStack.storage = {};
+  
+  // return the newStack
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  // count = this.count;
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  this.count--;
+  return this.storage[this.count];
+};
+
+stackMethods.size = function() {
+  return this.count > 0 ? this.count : 0;
+};

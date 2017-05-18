1fada9907112fc02aeae26d8e52dc21ddd03e925
Hack Reactor Student <student@hackreactor.com>
2017-05-17 18:43:08 -0700

    fixed all of functional-shared-stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  let newStack = {};
+  var newStack = {};
   newStack.storage = {};
   newStack.count = 0;
   extend(newStack, stackMethods);

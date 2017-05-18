6049453416339ad1f3b25a1c954ad31237ca0d42
Hack Reactor Student <student@hackreactor.com>
2017-05-17 18:35:03 -0700

    Finished functional-shared-stack until 'does declare variables'

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let newStack = {};
+  newStack.storage = {};
+  newStack.count = 0;
+  extend(newStack, stackMethods);
+  
+  return newStack;
+  
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  // count = this.count;
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  this.count--;
+  return this.storage[this.count];
+};
+
+stackMethods.size = function() {
+  return this.count > 0 ? this.count : 0;
+};

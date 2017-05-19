232c70c7fdcd017b304adfe090819f1cd5fc39bc
Hack Reactor Student <student@hackreactor.com>
2017-05-18 19:58:56 -0700

    pushing progress of linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null && list.tail === null) {
+      var beginningNode = Node(value);
+      list.head = beginningNode;
+      list.tail = beginningNode;
+    }
+    var newNode = Node(value);
+    
+    // if (list.head.next === null) {
+    //   list.head.next = newNode;
+    // }
+
+    list.tail = newNode;
+    
+    // #1 situation : list is empty
+      // we need to have one 'node' which can be considered both head and tail
+    // #2 situation : we have one 'node' so far
   };
 
   list.removeHead = function() {
+    // var finalVal = list.head.value;
+    // new head designated below
+    list.head = list.head.next;
+    return list.head.value;
   };
 
   list.contains = function(target) {
+    // start from head
+      // look up value
+        // if it exists
+        // return boolean === true;
+          // if it reaches end without matches, return false
   };
 
   return list;
@@ -27,3 +52,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
